5c4a10c11453c69fb7a9d77b5993eeda513a0e33
Ogbara Godwin <godwin.onisofien@andela.com>
2016-03-28 22:04:11 +0100

    change count to visit_count

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160328180301) do
+ActiveRecord::Schema.define(version: 20160328205518) do
 
   create_table "links", force: :cascade do |t|
     t.string   "full_url"
