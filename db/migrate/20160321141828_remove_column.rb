4aaa5e140284336e3d8b8721544397bbb7a0c4d2
Ogbara Godwin <godwin.onisofien@andela.com>
2016-03-28 11:50:48 +0100

    heroku migration issues

--- db/migrate/20160321141828_remove_column.rb
+++ db/migrate/20160321141828_remove_column.rb
@@ -1,5 +1,5 @@
-# class RemoveColumn < ActiveRecord::Migration
-#   def change
-#     remove_column :users, :links_id
-#   end
-# end
+class RemoveColumn < ActiveRecord::Migration
+  def change
+    remove_column :users, :links_id
+  end
+end
