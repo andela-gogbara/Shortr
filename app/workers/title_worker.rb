dbfafd6dea71b0004b83e0f0357855defedd21c5
Ogbara Godwin <godwin.onisofien@andela.com>
2016-03-28 17:31:16 +0100

    removes sidekiq

--- app/workers/title_worker.rb
+++ app/workers/title_worker.rb
@@ -1,11 +1,11 @@
-class TitleWorker
-  include Sidekiq::Worker
-
-  def perform(link_id)
-  link = Link.find(link_id)
-  agent = Mechanize.new
-  page = agent.get(link.full_url)
-  link.title = page.title
-  link.save
-  end
-end
+# class TitleWorker
+#   include Sidekiq::Worker
+#
+#   def perform(link_id)
+#   link = Link.find(link_id)
+#   agent = Mechanize.new
+#   page = agent.get(link.full_url)
+#   link.title = page.title
+#   link.save
+#   end
+# end
