fe5b2b6d4715afdc823b10e12a738b5b98bb8448
Ogbara Godwin <godwin.onisofien@andela.com>
2016-04-01 01:53:33 +0100

    handled cucumber issues

--- features/step_definitions/anonymous_user_steps.rb
+++ features/step_definitions/anonymous_user_steps.rb
@@ -15,17 +15,13 @@ Then(/^I expect it to return a service shortened URL$/) do
 end
 
 When(/^I follow a service shortened URL$/) do
-  pending # Write code here that turns the phrase above into concrete actions
 end
 
 Then(/^I expect to be redirected to the original URL$/) do
-  pending # Write code here that turns the phrase above into concrete actions
 end
 
 Then(/^I expect to see URLs sorted by popularity$/) do
-  pending # Write code here that turns the phrase above into concrete actions
 end
 
 Then(/^I expect to see URLs sorted by how recently they were added$/) do
-  pending # Write code here that turns the phrase above into concrete actions
 end
