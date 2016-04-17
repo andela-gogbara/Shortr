66edfa711e7fe5ff80b682300f0fb8ae5f1ab397
Ogbara Godwin <godwin.onisofien@andela.com>
2016-04-17 22:47:11 +0100

    test(redirect controller): add test for redirect controller

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -56,11 +56,12 @@ RSpec.describe LinksController, type: :controller, js: true do
     end
 
     context "with valid data" do
-      let(:valid_data) do attributes_for(
-        :link,
-        full_url: "full_url",
-        short_url: "short_url"
-      )
+      let(:valid_data) do
+        attributes_for(
+          :link,
+          full_url: "full_url",
+          short_url: "short_url"
+        )
       end
 
       before do
