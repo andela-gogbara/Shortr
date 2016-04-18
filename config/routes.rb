0dfc819cae2b43a087d0b0783b5be2e24abd314a
Ogbara Godwin <godwin.onisofien@andela.com>
2016-04-18 20:32:16 +0100

    style: defence review changes

--- config/routes.rb
+++ config/routes.rb
@@ -7,15 +7,15 @@ Rails.application.routes.draw do
     resources :links
   end
 
-  get "/", to: 'welcome#message', as: "root"
+  get "/", to: "welcome#message", as: "root"
 
-  get "signup", to: 'users#new', as: "signup"
+  get "signup", to: "users#new", as: "signup"
 
-  get "login", to: 'sessions#new', as: "login"
+  get "login", to: "sessions#new", as: "login"
 
-  get "logout", to: 'sessions#destroy', as: "logout"
+  get "logout", to: "sessions#destroy", as: "logout"
 
-  get "api_key", to: 'users#show_api', as: "api_key"
+  get "api_key", to: "users#show_api", as: "api_key"
 
-  get ":short_url", to: 'redirect#process_url'
+  get ":short_url", to: "redirect#process_url"
 end
